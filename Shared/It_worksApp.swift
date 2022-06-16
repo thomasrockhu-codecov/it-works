9e4f418189acbf9b3960618eba580aad4563ca27
Joe Becher <joe@codecov.io>
2022-06-16 07:23:00 -0400

    Initial

---
+++ Shared/It_worksApp.swift
@@ -0,0 +1,17 @@
+//
+//  It_worksApp.swift
+//  Shared
+//
+//  Created by Joe Becher on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct It_worksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
